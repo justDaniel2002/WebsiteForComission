bd51afa121b20f1e0d26f256cfb3c66d83959dc1
justDaniel2002 <todentsukanai@gmail.com>
2025-02-18 15:24:41 +0700

    minor-update

--- websiteforcomission/src/atoms/LinkButton.tsx
+++ websiteforcomission/src/atoms/LinkButton.tsx
@@ -22,19 +22,20 @@ const LinkButton = ({ pair }: LinkButtonParam) => {
         to={pair.link}
         className="my-5 mx-7 flex items-center"
       >
-        <Icon
-          icon={`${pair.icon}`}
-          className={`
-                transition-all duration-500 ease-in-out w-2/3
+        <div className="w-2/3">
+          <Icon
+            icon={`${pair.icon}`}
+            className={`
+                transition-all duration-500 ease-in-out min-w-[3vh]
             ${contentState ? "-translate-x-[2.5vh] text-xl" : "translate-x-[2.5vh] text-6xl"}`}
-        />
+          />
+        </div>
         <div
           className={`inline-block transition-all duration-500 ease-in-out w-3/5 text-sm font-medium
-                    ${
-                      contentState
-                        ? "opacity-100 translate-x-0"
-                        : "opacity-0 -translate-x-5"
-                    }`}
+                    ${contentState
+              ? "opacity-100 translate-x-0"
+              : "opacity-0 -translate-x-5"
+            }`}
         >
           {pair.content}
         </div>
--- websiteforcomission/src/components/Navbar.tsx
+++ websiteforcomission/src/components/Navbar.tsx
@@ -12,7 +12,8 @@ const Navbar = ({ direction, iconLinks }: NavbarParam) => {
   direction = direction || "vertical";
   return (
     <>
-      <div className={`${direction == "horizontal" ? "flex items-center" : "inline-block min-h-screen"} text-white bg-slate-900 w-[calc(7vw)] rounded-tr-xl rounded-br-xl`}>
+      <div className={`${direction == "horizontal" ? "flex items-center" 
+        : "inline-block min-h-screen"} text-white bg-slate-900 w-[calc(7vw+10px)] rounded-tr-xl rounded-br-xl`}>
         {iconLinks.map((pair, index) => (
           <div key={index}>
             <div><LinkButton pair={pair} /></div>
